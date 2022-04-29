cca12c7c1960c3fb8e7aa5d547ddcb2be59c3cde
KietHo1234 <104623160+KietHo1234@users.noreply.github.com>
2022-04-29 16:13:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+//Giai phiong trinh bac nhat ax + b = 0;
+$a = 5;
+$b = 6;
+if($a == 0){
+    if($b == 0){
+        echo 'phuong trinh co vo so nghiem';
+    }else{
+        echo 'phuong trinh vo nghiem';
+    }
+}else{
+    $x = - $b / $a;
+    echo  "pt co nghiem: $x";
+}
+?>
